7afaa2900f53cd8fddce872b68e407d77f436603
Alberto Suárez <alberto.suarez.ing@gmail.com>
2024-03-16 19:45:12 +0100

    Estilo de cursor en botón, rutas y vinculación de userView
    He vinculado la imagen del usuario con su url del userView y he modificado la ruta del userView

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,6 @@ import { UserViewComponent } from './pages/user-view/user-view.component';
 export const routes: Routes = [
     { path: "", pathMatch: "full", redirectTo: "userList" },
     { path: "userList", component: UserListComponent },
-    { path: "dashboard", component: UserListComponent, children: [
-        { path: "user/:_id", component: UserViewComponent }
-    ]},
+    { path: "user/:idusuario", component: UserViewComponent },
     { path: "**", redirectTo: "userList"}
 ];
