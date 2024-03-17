bf1179ae3438869635ed3b8d1f824b8e2c2bba17
Alberto Suárez <alberto.suarez.ing@gmail.com>
2024-03-17 09:31:20 +0100

    Maquetación del formulario 'userForm'
    He maquetado la estructura inicial del formulario 'userForm' y he inicializado las variables en el componente user-form.component.ts

--- src/app/pages/user-form/user-form.component.ts
+++ src/app/pages/user-form/user-form.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
 
+  type: string = 'new';
+  usersForm: FormGroup;
+
+  constructor() {
+    this.usersForm = new FormGroup({
+      first_name: new FormControl('', []),
+      last_name: new FormControl('', []),
+      email: new FormControl('', []),
+      username: new FormControl('', []),
+      password: new FormControl('', []),
+  }, []);
+  }
+
+  getDataForm() {
+    console.log(this.usersForm.value);
+  }
 }
